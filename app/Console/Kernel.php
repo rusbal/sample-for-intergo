b8d4299ed275b053e8f5b56ace0f484a8c04164f
Raymond Usbal <raymond@philippinedev.com>
2017-01-18 09:58:36 +0800

    added comments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,14 +35,40 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        /**
+         * begin-request checks the following:
+         *
+         * 1. Amazon MWS exists but no _GET_MERCHANT_LISTINGS_DATA_ request on Table: amazon_request_histories
+         * 2. _GET_MERCHANT_LISTINGS_DATA_ exists but no _GET_AFN_INVENTORY_DATA_ on Table: amazon_request_histories
+         *
+         * If so, it initiates a request and writes it to Table: amazon_request_queues with a class=report.
+         * It does not insert a new request if there is an existing similar request on queue.
+         */
         $schedule->command('skubright:begin-request')
                  ->everyMinute();
 
-        $schedule->command('skubright:process-queue')
-                 ->everyMinute();
-
+        /**
+         * update-data-request initiates _GET_MERCHANT_LISTINGS_DATA_ and _GET_AFN_INVENTORY_DATA_ requests
+         * and inserts it to Table: amazon_request_queues with a class=report-update.
+         */
         $schedule->command('skubright:update-data-request')
                  ->hourly();
+
+        /**
+         * process-queue reads Table: amazon_request_queues and follows through Amazon using request_id
+         * If the report is available, it fetches the flat-file report, parses it,
+         * and saves to Table: amazon_merchant_listings.
+         *
+         * If class == report, it inserts
+         * If class == report-update, it updates
+         *
+         * The flat file report is saved to storage/amazon/ directory.
+         *
+         * If status == _DONE_, request is deleted from queue and written to Table: amazon_request_histories.
+         * If status == _CANCELLED, request is deleted from queue.
+         */
+        $schedule->command('skubright:process-queue')
+                 ->everyMinute();
     }
 
     /**
