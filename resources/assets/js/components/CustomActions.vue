bafc17cae3684db70105fce50c1043fd4acc615f
Raymond Usbal <raymond@philippinedev.com>
2017-01-22 13:21:44 +0800

    show busy button

--- resources/assets/js/components/CustomActions.vue
+++ resources/assets/js/components/CustomActions.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="custom-actions">
-        <button v-if="rowData.will_monitor" class="ui green button" @click="itemAction('uncheck-item', rowData, rowIndex)"><i class="checkmark icon"></i></button>
-        <button v-else class="ui button" @click="itemAction('check-item', rowData, rowIndex)"><i class="square outline icon"></i></button>
+
+        <button v-if="rowData.will_monitor === 1"
+                class="ui green button"
+                @click="itemAction('uncheck-item', rowData, rowIndex)">
+            <i class="checkmark icon"></i>
+        </button>
+
+        <button v-else-if="rowData.will_monitor === 0"
+                class="ui button"
+                @click="itemAction('check-item', rowData, rowIndex)">
+            <i class="square outline icon"></i>
+        </button>
+
+        <button v-else class="ui yellow button">
+            <i class="spinner icon"></i>
+        </button>
+
     </div>
 </template>
 
@@ -19,14 +34,18 @@
     methods: {
       itemAction (action, data, index) {
 
+        let hold_monitor = data.will_monitor
+        data.will_monitor = null
+
         axios.patch(
           Laravel.route('', data.id),
-          { will_monitor: data.will_monitor ? 0 : 1 }
-        ).then((response) => {
-          if (response.data.success) {
-            data.will_monitor = ! data.will_monitor
-          }
-        });
+          { will_monitor: hold_monitor ? 0 : 1 }
+
+        ).then((response) => data.will_monitor = response.data.success
+            ? (hold_monitor ? 0 : 1)
+            : hold_monitor
+
+        ).catch((reponse) => data.will_monitor = hold_monitor);
 
       }
     }
