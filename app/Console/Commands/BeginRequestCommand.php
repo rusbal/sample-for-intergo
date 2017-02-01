5919639ac1f155c5c5305b692b82b41713ecee5d
Raymond Usbal <raymond@philippinedev.com>
2017-02-01 10:43:33 +0800

    removed old code

--- app/Console/Commands/BeginRequestCommand.php
+++ app/Console/Commands/BeginRequestCommand.php
@@ -111,31 +111,4 @@ class BeginRequestCommand extends Command
         // Remove from reports to request
         return array_diff($reports, $onHistory);
     }
-
-    /**
-     * Seems this is no longer needed.
-     * Its purpose is to initiate a request which should be automatically initiated anyway
-     * unless the amazon_mws_auth is not passed which was the problem this code solves.
-     * Now that we pass amazon_mws_auth, there is no longer need for this.
-     */
-    private function initiateOnly()
-    {
-        $queue = AmazonRequestQueue::where('request_id', '')->get();
-
-        foreach ($queue as $item) {
-            Auth::loginUsingId($item->store_name);
-
-            $result = RequestReport::initiate($item);
-
-            if ($result) {
-                $this->info("*** Successfully started request for: {$item->store_name}");
-            } else {
-                $this->info("--- Failed to started request for: {$item->store_name}");
-            }
-
-            Auth::logout();
-        }
-
-        $this->info('Job: Start report requests executed.');
-    }
 }
