7fc433cba0eb3cb95bb306b4a1b0d0f3149f3a18
Raymond Usbal <raymond@philippinedev.com>
2017-02-03 01:38:42 +0800

    Model to EloquentModel

--- app/AmazonUnsuppressedInventory.php
+++ app/AmazonUnsuppressedInventory.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
 
-class AmazonUnsuppressedInventory extends Model
+class AmazonUnsuppressedInventory extends EloquentModel
 {
     protected $fillable = [
         'sku',
