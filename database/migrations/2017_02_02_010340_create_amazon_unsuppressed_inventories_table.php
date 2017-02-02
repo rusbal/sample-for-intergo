1fb79c1c85b21f6afd90075a64d25ecb70ae4baf
Raymond Usbal <raymond@philippinedev.com>
2017-02-02 09:23:59 +0800

    dash to underscore

--- database/migrations/2017_02_02_010340_create_amazon_unsuppressed_inventories_table.php
+++ database/migrations/2017_02_02_010340_create_amazon_unsuppressed_inventories_table.php
@@ -19,21 +19,21 @@ class CreateAmazonUnsuppressedInventoriesTable extends Migration
             $table->integer('sku')->unsigned();
             $table->string('fnsku');
             $table->string('asin');
-            $table->string('product-name');
+            $table->string('product_name');
             $table->string('condition');
-            $table->decimal('your-price', 9, 2);
-            $table->string('mfn-listing-exists');
-            $table->string('mfn-fulfillable-quantity');
-            $table->string('afn-listing-exists');
-            $table->integer('afn-warehouse-quantity');
-            $table->integer('afn-fulfillable-quantity');
-            $table->integer('afn-unsellable-quantity');
-            $table->integer('afn-reserved-quantity');
-            $table->integer('afn-total-quantity');
-            $table->decimal('per-unit-volume', 9, 2);
-            $table->integer('afn-inbound-working-quantity');
-            $table->integer('afn-inbound-shipped-quantity');
-            $table->integer('afn-inbound-receiving-quantity');
+            $table->decimal('your_price', 9, 2);
+            $table->string('mfn_listing_exists');
+            $table->string('mfn_fulfillable_quantity');
+            $table->string('afn_listing_exists');
+            $table->integer('afn_warehouse_quantity');
+            $table->integer('afn_fulfillable_quantity');
+            $table->integer('afn_unsellable_quantity');
+            $table->integer('afn_reserved_quantity');
+            $table->integer('afn_total_quantity');
+            $table->decimal('per_unit_volume', 9, 2);
+            $table->integer('afn_inbound_working_quantity');
+            $table->integer('afn_inbound_shipped_quantity');
+            $table->integer('afn_inbound_receiving_quantity');
 
             $table->timestamps();
 
