02ed62fe3c1a6b42e169beb3f9793594040b6960
Raymond Usbal <raymond@philippinedev.com>
2017-02-02 09:47:35 +0800

    fillable

--- app/AmazonUnsuppressedInventory.php
+++ app/AmazonUnsuppressedInventory.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class AmazonUnsuppressedInventory extends Model
 {
-    //
+    protected $fillable = [
+        'sku',
+        'fnsku',
+        'asin',
+        'product_name',
+        'condition',
+        'your_price',
+        'mfn_listing_exists',
+        'mfn_fulfillable_quantity',
+        'afn_listing_exists',
+        'afn_warehouse_quantity',
+        'afn_fulfillable_quantity',
+        'afn_unsellable_quantity',
+        'afn_reserved_quantity',
+        'afn_total_quantity',
+        'per_unit_volume',
+        'afn_inbound_working_quantity',
+        'afn_inbound_shipped_quantity',
+        'afn_inbound_receiving_quantity',
+    ];
 }
