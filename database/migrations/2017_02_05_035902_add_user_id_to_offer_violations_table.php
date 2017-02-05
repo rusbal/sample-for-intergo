8ae96e83d35a089b6ba8ece0a903feefd30d44ac
Raymond Usbal <raymond@philippinedev.com>
2017-02-05 12:03:02 +0800

    add user_id to price and offer violations tables

---
+++ database/migrations/2017_02_05_035902_add_user_id_to_offer_violations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToOfferViolationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('amazon_merchant_listing_offer_violations', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+
+            $table->index('user_id', 'user_id_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('amazon_merchant_listing_offer_violations', function (Blueprint $table) {
+            $table->dropIndex('user_id_index');
+
+            $table->dropColumn('user_id');
+        });
+    }
+}
