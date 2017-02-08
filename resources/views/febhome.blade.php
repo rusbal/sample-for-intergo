9a671bfe5230ef142e070cc5dd88754f5b2e7f25
Raymond Usbal <raymond@philippinedev.com>
2017-02-08 22:32:23 +0800

    testimonials

--- resources/views/febhome.blade.php
+++ resources/views/febhome.blade.php
@@ -22,7 +22,7 @@
                 <img src="/img/sku-selection-page.png" alt="">
             </div>
 
-            <div class="col-lg-3 col-lg-offset-0 col-md-6 col-md-offset-0 col-sm-10 col-sm-offset-1">
+            <div class="col-lg-3 col-lg-offset-0 col-md-6 col-md-offset-0 col-sm-10 col-sm-offset-1 p-b-40">
                 <h1>Praesent placerat</h1>
                 <p>To copy a specific application window, press and hold Command-Control-Shift-4 then tap on the Spacebar. The cursor will change to a camera, which you can move around the screen. As you move the cursor over an application window, the window will be highlighted.</p>
 
@@ -42,6 +42,54 @@
                 </ol>
             </div>
         </div>
+    </div>
 
+    <div class="container-fluid home-block-reviews" style="background-color:#87d9bf; min-height:300px; color: white;">
+
+        <h2 class="text-center p-tb-20">See what other SKUBright Users are saying</h2>
+
+        <div class="row p-b-40">
+            <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+
+                <div class="row">
+
+                    <div class="col-sm-4">
+                        <figure>
+                            <blockquote class="p-12">To capture a specific application window, press and hold Command-Shift-4 then tap on the Spacebar. The cursor will change to a camera, and you can move it around the screen.</blockquote>
+                            <footer>
+                                <cite class="author"> James Smith </cite>
+                                <cite class="info"> Owner, Ipsum Corp. </cite>
+                            </footer>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <figure>
+                            <blockquote class="p-12">Lorem ipsum window, press and hold Command-Shift-4 then tap on the Spacebar. The cursor will change to a camera, and you can move it around the screen.</blockquote>
+                            <footer>
+                                <cite class="author"> Mary Olberg </cite>
+                                <cite class="info"> Banana Corp. </cite>
+                            </footer>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <figure>
+                            <blockquote class="p-12">Print a monkey specific application window, press and hold Command-Shift-4 then tap on the Spacebar. The cursor will change to a camera, and you can move it around the screen.</blockquote>
+                            <footer>
+                                <cite class="author"> Henry Guttenberg </cite>
+                                <cite class="info"> Printing Corp. </cite>
+                            </footer>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-4">
+                    </div>
+                    <div class="col-sm-4">
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 @endsection
